0d96f7cf776d5dbe208f6b82d5e159972d67d572
MIGUEL <anthonymiguelochoa03@gmail.com>
2026-04-09 03:17:12 -0700

    Single Tap, Pinch Zoom In/Out

---
+++ Assets/Scripts/MobileInputs.cs
@@ -0,0 +1,71 @@
+using UnityEngine;
+
+public class MobileInputs : MonoBehaviour
+{
+    float lastDistance;
+
+    [SerializeField]
+    public GameObject cube;
+    public Material material;
+
+
+    void Update()
+    {
+        SingleTap();
+        Swipe();
+    }
+    void SingleTap()
+    {
+        // Single Tap
+        if (Input.touchCount == 1)
+        {
+            Touch t = Input.GetTouch(0); // first finger
+
+            if (t.phase == TouchPhase.Began)
+            {
+                Debug.Log("Single Tap Detected");
+
+            }
+        }
+    }
+    void Swipe()
+    {
+        if (Input.touchCount == 2)
+        {
+            Touch t0 = Input.GetTouch(0); // first finger
+            Touch t1 = Input.GetTouch(1); // second finger
+
+            float currentDistance = Vector2.Distance(t0.position, t1.position);
+
+            if (t1.phase == TouchPhase.Began)
+            {
+                lastDistance = currentDistance;
+                return;
+            }
+            float difference = currentDistance - lastDistance;
+
+            if (Mathf.Abs(difference) > 5f)
+            {
+                Vector3 scale = cube.transform.localScale;
+
+                if (difference > 0)
+                {
+                    Debug.Log("Zoom In");
+                    scale += Vector3.one * 0.1f;
+                }
+                else
+                {
+                    Debug.Log("Zoom Out");
+                    scale -= Vector3.one * 0.1f;
+                }
+                scale.x = Mathf.Clamp(scale.x, 0.1f, 3f);
+                scale.y = Mathf.Clamp(scale.y, 0.1f, 3f);
+                scale.z = Mathf.Clamp(scale.z, 0.1f, 3f);
+
+                cube.transform.localScale = scale;
+            }
+            lastDistance = currentDistance;
+        }
+    }
+
+}
